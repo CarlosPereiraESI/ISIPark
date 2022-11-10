651ba81295d183c6d7b222146f0add28b3988a1e
sergiojoao3240 <sergiojoao3240@gmail.com>
2022-11-10 14:03:41 +0000

    dashboard

--- app/src/main/java/com/example/isipark/DashboardActivity.kt
+++ app/src/main/java/com/example/isipark/DashboardActivity.kt
@@ -1,11 +1,19 @@
 package com.example.isipark
 
+import MySimpleArrayAdapterObjects
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ListView
+import sector
 
 class DashboardActivity : AppCompatActivity() {
+
+    val listView: ListView by lazy{
+        findViewById<ListView>(R.id.dashboard_list_sectors)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dashboard)
@@ -17,5 +25,10 @@ class DashboardActivity : AppCompatActivity() {
             val intent = Intent(this@DashboardActivity, CodeActivity::class.java)
             startActivity(intent)
         }
+
+        val values = mutableListOf<sector>(sector("Sector T", "Normal: 50", "Eletric: 3", "Motorcycle: 10", "R.Mobility: 1"), sector("Sector D", "Normal: 20", "Eletric: 1", "Motorcycle: 5", "R.Mobility: 0"), sector("Sector G", "Normal: 10", "Eletric: 0", "Motorcycle: 1", "R.Mobility: 2"))
+        //var adapter = ArrayAdapter(this, android.R.layout.simple_expandable_list_item_1, values)
+        var adapter = MySimpleArrayAdapterObjects(this, R.layout.layout_sector_dash , values)
+        listView.adapter = adapter
     }
 }
---
+++ app/src/main/java/com/example/isipark/sector.kt
@@ -0,0 +1,3 @@
+
+class sector(val sector:String, val normal:String, val eletric:String, val motorcycle:String, val r_mobility:String) {
+}
