bcb9adc92e932dfe18378c2d325b23e4d8370248
Sérgio Gonçalves <sergiojoao3240@gmail.com>
2022-11-29 12:32:51 +0000

    conexao entre mockups

--- app/src/main/java/com/example/isipark/ReportsMessagesActivity.kt
+++ app/src/main/java/com/example/isipark/ReportsMessagesActivity.kt
@@ -1,11 +1,22 @@
 package com.example.isipark
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class ReportsMessagesActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_reports_messages)
+
+        val sendBtn = findViewById<Button>(R.id.report_send_btn)
+
+        sendBtn.setOnClickListener {
+            val intent = Intent(this, SendNotificationActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
--- app/src/main/java/com/example/isipark/SendNotificationActivity.kt
+++ app/src/main/java/com/example/isipark/SendNotificationActivity.kt
@@ -1,11 +1,20 @@
 package com.example.isipark
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class SendNotificationActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_send_notification)
+
+        val back = findViewById<Button>(R.id.send_noti_back_btn)
+
+        back.setOnClickListener {
+            val intent = Intent(this, ReportsMessagesActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
