a10000df5700aa2db1f66ed5c9b9ecb125002662
CarlosPereiraESI <jocause@hotmail.com>
2023-01-10 15:28:21 +0000

    All Models

---
+++ app/src/main/java/com/example/isipark/Model/PersonalData.kt
@@ -0,0 +1,10 @@
+package com.example.isipark.Model
+
+import java.util.Date
+
+class PersonalData(val number: Int, val name: String, val CP4: Int,
+                   val CP3: Int, val street: String, val contact: Int, val nif: Int,
+                   val email: String, val password: String, val birthDate: Date,
+                   val gender: String, val userType: String, val payment: Boolean,
+                   val userID: Int) {
+}
