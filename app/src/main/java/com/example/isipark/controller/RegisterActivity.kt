167724940121eb5db334dac37474c50716d6cb0c
CarlosPereiraESI <jocause@hotmail.com>
2023-01-15 04:27:33 +0000

    Added Back Button on Register Page

--- app/src/main/java/com/example/isipark/controller/RegisterActivity.kt
+++ app/src/main/java/com/example/isipark/controller/RegisterActivity.kt
@@ -14,12 +14,16 @@ class RegisterActivity : AppCompatActivity() {
         setContentView(R.layout.activity_register)
 
         val createBtn = findViewById<Button>(R.id.register_create_btn)
-
         val email = findViewById<EditText>(R.id.register_email_et)
         val password = findViewById<EditText>(R.id.register_password_et)
-        val confpassword =findViewById<EditText>(R.id.register_conf_password_et)
+        val confpassword = findViewById<EditText>(R.id.register_conf_password_et)
+        val back = findViewById<Button>(R.id.back_register_btn)
 
-
+        back.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+        
         createBtn.setOnClickListener {
             if ( email.text.toString() == "" || password.text.toString() == "" || confpassword.text.toString() == ""){
                 Toast.makeText(this,"It has empty fields!", Toast.LENGTH_SHORT).show()
@@ -32,17 +36,13 @@ class RegisterActivity : AppCompatActivity() {
                     } else {
                         Toast.makeText(this,"Invalid E-mail, try again!", Toast.LENGTH_SHORT).show()
                     }
-
                 }
                 else {
                     Toast.makeText(this, "The passwords don't match!", Toast.LENGTH_SHORT).show()
                 }
             }
         }
-
-
     }
-
     private fun isCorrect(string: String): Boolean {
         return string.matches("(([a-zA-Z0-9.])+[@][a-z]+[.][a-z]+)".toRegex())
     }
