3401258b6ef00d7cb9a6f91f37fb7d7cb2ac81a9
Sérgio Gonçalves <sergiojoao3240@gmail.com>
2023-01-16 17:26:21 +0000

    Add vehicles resolved

--- app/src/main/java/com/example/isipark/controller/Add_VehicleActivity.kt
+++ app/src/main/java/com/example/isipark/controller/Add_VehicleActivity.kt
@@ -3,7 +3,11 @@ package com.example.isipark.controller
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import android.widget.Button
+import android.widget.Spinner
 import com.example.isipark.R
 
 class Add_VehicleActivity : AppCompatActivity() {
@@ -14,6 +18,21 @@ class Add_VehicleActivity : AppCompatActivity() {
         //Buttons page
         val save = findViewById<Button>(R.id.addVehicleSave)
         val back = findViewById<Button>(R.id.addVehicle_back)
+        val spinner = findViewById<Spinner>(R.id.addvehicletype)
+
+        if(spinner != null){
+            val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item,
+                listOf("normal", "eletric", "motorcycle", "reduce mobility"))
+            spinner.adapter = adapter
+        }
+
+        spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
+            override fun onItemSelected(parent: AdapterView<*>, view: View, position: Int, id: Long) {
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>) {
+            }
+        }
 
         // Button Save
         save.setOnClickListener{
